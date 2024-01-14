77a1ac521e119663af459f4ebf4359925b942b94
JanKnd <134882705+JanKnd@users.noreply.github.com>
2024-01-14 14:32:20 +0100

    done

--- 3d_phase_portraits/main.py
+++ 3d_phase_portraits/main.py
@@ -1,16 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cmath
-
+import math
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 
 # f(x) {x ∈ R   ->   z(x)=x+f(x)i {x ∈ C
+def zeta_funtion(z:complex, degree_of_percition: int):
+    output: complex = 0
+    n = 1
+    for i in range(degree_of_percition):
+        output += 1/(cmath.exp(z*math.log(n,10)))
+        n += 1
+    return(output)
+    
 def complex_function(z:complex):   
-    return(z**0.5) 
+    return((1/1**z)+(1/2**z)+(1/3**z)+(1/4**z)+(1/5**z)+(1/6**z)) 
 
-X_real = np.arange(-5, 5, 0.01)
-Y_imag = np.arange(-5, 5, 0.01)
+X_real = np.arange(-40, 40, 0.01)
+Y_imag = np.arange(-2, 2, 0.01)
 
 X,Y = np.meshgrid(X_real,Y_imag)
 
@@ -38,7 +46,8 @@ xy_comp = np.add(x_comp_real,y_comp_imag)
 #xy_comp_res = np.tan(xy_comp)
 xy_comp_res = np.ndarray(xy_comp.shape,dtype=complex)
 for i in range(flat_len):
-    xy_comp_res[i] = complex_function(xy_comp[i])
+    xy_comp_res[i] = zeta_funtion(xy_comp[i],5)
+    #complex_function(xy_comp[i])
 
 #z coordinates = abs val of complex number
 xy_abs_val = np.abs(xy_comp_res)
@@ -64,6 +73,7 @@ surf = ax.plot_surface(X, Y, xy_abs_val, facecolors=plt.cm.gist_rainbow(norm(xy_
 ax.set_xlabel('real')
 ax.set_ylabel('imaginary')
 ax.set_zlabel('absolute value')
+ax.set_zlim(0, 6)
 
 m = cm.ScalarMappable(cmap=plt.cm.gist_rainbow, norm=norm)
 plt.colorbar(m)
